e387a1f1caae898be0a48364cd72cfba71cfb82e
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2021-12-22 01:24:53 +0900

    Update setup.bat

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,6 +1,6 @@
 mkdir C:\Users\wwwto\π\setup
 
-bitsadmin /transfer download https://github.com/apple-pi-yhhhh/topitopi.exe/blob/main/test/setup/test.zip?raw=true C:\Users\wwwto\π\setup.zip
+bitsadmin /transfer download https://github.com/apple-pi-yhhhh/topitopi.exe/blob/main/setup/main.zip?raw=true C:\Users\wwwto\π\setup.zip
 
 cd C:\Users\wwwto\π
 
